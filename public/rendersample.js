019572bd07b1be48b0eacccc88e1bdf115fff95c
tourn <latzer.daniel@gmail.com>
2016-04-20 20:22:09 +0200

    Extract sample data

---
+++ public/rendersample.js
@@ -0,0 +1,799 @@
+var sample = {"players":{
+  "aa":{
+  "provider": {
+      "name": "Dota 2",
+      "appid": 570,
+      "version": 44,
+      "timestamp": 1461085219
+    },
+  "map": {
+      "name": "dota",
+      "matchid": 0,
+      "game_time": 259,
+      "clock_time": 116,
+      "daytime": true,
+      "nightstalker_night": false,
+      "game_state": "DOTA_GAMERULES_STATE_GAME_IN_PROGRESS",
+      "win_team": "none",
+      "customgamename": "",
+      "ward_purchase_cooldown": 0
+    },
+  "player": {
+      "steamid": "76561198007555251",
+      "name": "tourn",
+      "activity": "playing",
+      "kills": 3,
+      "deaths": 2,
+      "assists": 7,
+      "last_hits": 124,
+      "denies": 7,
+      "kill_streak": 2,
+      "team_name": "radiant",
+      "gold": 322,
+      "gold_reliable": 322,
+      "gold_unreliable": 0,
+      "gpm": 420,
+      "xpm": 999
+    },
+  "hero": {
+      "id": 36,
+      "name": "npc_dota_hero_undying",
+      "level": 2,
+      "alive": false,
+      "respawn_seconds": 20,
+      "buyback_cost": 130,
+      "buyback_cooldown": 0,
+      "health": 420,
+      "max_health": 503,
+      "health_percent": 85,
+      "mana": 100,
+      "max_mana": 338,
+      "mana_percent": 35,
+      "silenced": false,
+      "stunned": false,
+      "disarmed": false,
+      "magicimmune": false,
+      "hexed": false,
+      "muted": false,
+      "break": false,
+      "has_debuff": false
+    },
+  "abilities": {
+      "ability0": {
+            "name": "necrolyte_death_pulse",
+            "level": 1,
+            "can_cast": true,
+            "passive": false,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability1": {
+            "name": "necrolyte_heartstopper_aura",
+            "level": 0,
+            "can_cast": false,
+            "passive": true,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability2": {
+            "name": "necrolyte_sadist",
+            "level": 0,
+            "can_cast": false,
+            "passive": true,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability3": {
+            "name": "necrolyte_reapers_scythe",
+            "level": 0,
+            "can_cast": false,
+            "passive": false,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": true
+          },
+      "attributes": {
+            "level": 0
+          }
+    },
+  "items": {
+      "slot0": {
+            "name": "item_branches",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false
+          },
+      "slot1": {
+            "name": "item_mantle",
+            "passive": true
+          },
+      "slot2": {
+            "name": "item_clarity",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 1
+          },
+      "slot3": {
+            "name": "item_tpscroll",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 1
+          },
+      "slot4": {
+            "name": "empty",
+          },
+      "slot5": {
+            "name": "item_tango",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 3
+          },
+      "stash0": {
+            "name": "item_circlet",
+            "passive": true
+          },
+      "stash1": {
+            "name": "item_branches",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false
+          },
+      "stash2": {
+            "name": "empty"
+          },
+      "stash3": {
+            "name": "empty"
+          },
+      "stash4": {
+            "name": "empty"
+          },
+      "stash5": {
+            "name": "empty"
+          }
+    }
+}, 
+  "bb":{
+  "provider": {
+      "name": "Dota 2",
+      "appid": 570,
+      "version": 44,
+      "timestamp": 1461085219
+    },
+  "map": {
+      "name": "dota",
+      "matchid": 0,
+      "game_time": 259,
+      "clock_time": 116,
+      "daytime": true,
+      "nightstalker_night": false,
+      "game_state": "DOTA_GAMERULES_STATE_GAME_IN_PROGRESS",
+      "win_team": "none",
+      "customgamename": "",
+      "ward_purchase_cooldown": 0
+    },
+  "player": {
+      "steamid": "76561198007555251",
+      "name": "Mighty Hans",
+      "activity": "playing",
+      "kills": 3,
+      "deaths": 2,
+      "assists": 7,
+      "last_hits": 124,
+      "denies": 7,
+      "kill_streak": 2,
+      "team_name": "radiant",
+      "gold": 322,
+      "gold_reliable": 0,
+      "gold_unreliable": 80,
+      "gpm": 420,
+      "xpm": 999
+    },
+  "hero": {
+      "id": 36,
+      "name": "npc_dota_hero_sven",
+      "level": 24,
+      "alive": true,
+      "respawn_seconds": 0,
+      "buyback_cost": 130,
+      "buyback_cooldown": 122,
+      "health": 420,
+      "max_health": 503,
+      "health_percent": 85,
+      "mana": 100,
+      "max_mana": 338,
+      "mana_percent": 35,
+      "silenced": false,
+      "stunned": false,
+      "disarmed": false,
+      "magicimmune": false,
+      "hexed": false,
+      "muted": false,
+      "break": false,
+      "has_debuff": false
+    },
+  "abilities": {
+      "ability0": {
+            "name": "necrolyte_death_pulse",
+            "level": 1,
+            "can_cast": true,
+            "passive": false,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability1": {
+            "name": "necrolyte_heartstopper_aura",
+            "level": 0,
+            "can_cast": false,
+            "passive": true,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability2": {
+            "name": "necrolyte_sadist",
+            "level": 0,
+            "can_cast": false,
+            "passive": true,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability3": {
+            "name": "necrolyte_reapers_scythe",
+            "level": 0,
+            "can_cast": false,
+            "passive": false,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": true
+          },
+      "attributes": {
+            "level": 0
+          }
+    },
+  "items": {
+      "slot0": {
+            "name": "item_branches",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false
+          },
+      "slot1": {
+            "name": "item_mantle",
+            "passive": true
+          },
+      "slot2": {
+            "name": "item_clarity",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 1
+          },
+      "slot3": {
+            "name": "item_tpscroll",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 1
+          },
+      "slot4": {
+            "name": "empty",
+          },
+      "slot5": {
+            "name": "item_tango",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 3
+          },
+      "stash0": {
+            "name": "item_circlet",
+            "passive": true
+          },
+      "stash1": {
+            "name": "item_branches",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false
+          },
+      "stash2": {
+            "name": "empty"
+          },
+      "stash3": {
+            "name": "empty"
+          },
+      "stash4": {
+            "name": "empty"
+          },
+      "stash5": {
+            "name": "empty"
+          }
+    }
+}, 
+  "cc":{
+  "provider": {
+      "name": "Dota 2",
+      "appid": 570,
+      "version": 44,
+      "timestamp": 1461085219
+    },
+  "map": {
+      "name": "dota",
+      "matchid": 0,
+      "game_time": 259,
+      "clock_time": 116,
+      "daytime": true,
+      "nightstalker_night": false,
+      "game_state": "DOTA_GAMERULES_STATE_GAME_IN_PROGRESS",
+      "win_team": "none",
+      "customgamename": "",
+      "ward_purchase_cooldown": 0
+    },
+  "player": {
+      "steamid": "76561198007555251",
+      "name": "Zippy",
+      "activity": "playing",
+      "kills": 3,
+      "deaths": 2,
+      "assists": 7,
+      "last_hits": 124,
+      "denies": 7,
+      "kill_streak": 2,
+      "team_name": "radiant",
+      "gold": 322,
+      "gold_reliable": 0,
+      "gold_unreliable": 80,
+      "gpm": 420,
+      "xpm": 999
+    },
+  "hero": {
+      "id": 36,
+      "name": "npc_dota_hero_earth_spirit",
+      "level": 24,
+      "alive": true,
+      "respawn_seconds": 0,
+      "buyback_cost": 130,
+      "buyback_cooldown": 122,
+      "health": 420,
+      "max_health": 503,
+      "health_percent": 85,
+      "mana": 100,
+      "max_mana": 338,
+      "mana_percent": 35,
+      "silenced": false,
+      "stunned": false,
+      "disarmed": false,
+      "magicimmune": false,
+      "hexed": false,
+      "muted": false,
+      "break": false,
+      "has_debuff": false
+    },
+  "abilities": {
+      "ability0": {
+            "name": "necrolyte_death_pulse",
+            "level": 1,
+            "can_cast": true,
+            "passive": false,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability1": {
+            "name": "necrolyte_heartstopper_aura",
+            "level": 0,
+            "can_cast": false,
+            "passive": true,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability2": {
+            "name": "necrolyte_sadist",
+            "level": 0,
+            "can_cast": false,
+            "passive": true,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability3": {
+            "name": "necrolyte_reapers_scythe",
+            "level": 0,
+            "can_cast": false,
+            "passive": false,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": true
+          },
+      "attributes": {
+            "level": 0
+          }
+    },
+  "items": {
+      "slot0": {
+            "name": "item_branches",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false
+          },
+      "slot1": {
+            "name": "item_mantle",
+            "passive": true
+          },
+      "slot2": {
+            "name": "item_clarity",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 1
+          },
+      "slot3": {
+            "name": "item_tpscroll",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 1
+          },
+      "slot4": {
+            "name": "empty",
+          },
+      "slot5": {
+            "name": "item_tango",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 3
+          },
+      "stash0": {
+            "name": "item_circlet",
+            "passive": true
+          },
+      "stash1": {
+            "name": "item_branches",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false
+          },
+      "stash2": {
+            "name": "empty"
+          },
+      "stash3": {
+            "name": "empty"
+          },
+      "stash4": {
+            "name": "empty"
+          },
+      "stash5": {
+            "name": "empty"
+          }
+    }
+}, 
+  "zz":{
+  "provider": {
+      "name": "Dota 2",
+      "appid": 570,
+      "version": 44,
+      "timestamp": 1461085219
+    },
+  "map": {
+      "name": "dota",
+      "matchid": 0,
+      "game_time": 259,
+      "clock_time": 116,
+      "daytime": true,
+      "nightstalker_night": false,
+      "game_state": "DOTA_GAMERULES_STATE_GAME_IN_PROGRESS",
+      "win_team": "none",
+      "customgamename": "",
+      "ward_purchase_cooldown": 0
+    },
+  "player": {
+      "steamid": "76561198007555251",
+      "name": "blub_is_zirp",
+      "activity": "playing",
+      "kills": 3,
+      "deaths": 2,
+      "assists": 7,
+      "last_hits": 124,
+      "denies": 7,
+      "kill_streak": 2,
+      "team_name": "radiant",
+      "gold": 322,
+      "gold_reliable": 0,
+      "gold_unreliable": 80,
+      "gpm": 420,
+      "xpm": 999
+    },
+  "hero": {
+      "id": 36,
+      "name": "npc_dota_hero_windrunner",
+      "level": 24,
+      "alive": true,
+      "respawn_seconds": 0,
+      "buyback_cost": 130,
+      "buyback_cooldown": 122,
+      "health": 420,
+      "max_health": 503,
+      "health_percent": 85,
+      "mana": 100,
+      "max_mana": 338,
+      "mana_percent": 35,
+      "silenced": false,
+      "stunned": false,
+      "disarmed": false,
+      "magicimmune": false,
+      "hexed": false,
+      "muted": false,
+      "break": false,
+      "has_debuff": false
+    },
+  "abilities": {
+      "ability0": {
+            "name": "necrolyte_death_pulse",
+            "level": 1,
+            "can_cast": true,
+            "passive": false,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability1": {
+            "name": "necrolyte_heartstopper_aura",
+            "level": 0,
+            "can_cast": false,
+            "passive": true,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability2": {
+            "name": "necrolyte_sadist",
+            "level": 0,
+            "can_cast": false,
+            "passive": true,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability3": {
+            "name": "necrolyte_reapers_scythe",
+            "level": 0,
+            "can_cast": false,
+            "passive": false,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": true
+          },
+      "attributes": {
+            "level": 0
+          }
+    },
+  "items": {
+      "slot0": {
+            "name": "item_branches",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false
+          },
+      "slot1": {
+            "name": "item_mantle",
+            "passive": true
+          },
+      "slot2": {
+            "name": "item_clarity",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 1
+          },
+      "slot3": {
+            "name": "item_tpscroll",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 1
+          },
+      "slot4": {
+            "name": "empty",
+          },
+      "slot5": {
+            "name": "item_tango",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 3
+          },
+      "stash0": {
+            "name": "item_circlet",
+            "passive": true
+          },
+      "stash1": {
+            "name": "item_branches",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false
+          },
+      "stash2": {
+            "name": "empty"
+          },
+      "stash3": {
+            "name": "empty"
+          },
+      "stash4": {
+            "name": "empty"
+          },
+      "stash5": {
+            "name": "empty"
+          }
+    }
+}, 
+  "yy": {
+  "provider": {
+      "name": "Dota 2",
+      "appid": 570,
+      "version": 44,
+      "timestamp": 1461085219
+    },
+  "map": {
+      "name": "dota",
+      "matchid": 0,
+      "game_time": 259,
+      "clock_time": 116,
+      "daytime": true,
+      "nightstalker_night": false,
+      "game_state": "DOTA_GAMERULES_STATE_GAME_IN_PROGRESS",
+      "win_team": "none",
+      "customgamename": "",
+      "ward_purchase_cooldown": 0
+    },
+  "player": {
+      "steamid": "76561198007555251",
+      "name": "some jerk with a regrettably long name",
+      "activity": "playing",
+      "kills": 3,
+      "deaths": 2,
+      "assists": 7,
+      "last_hits": 124,
+      "denies": 7,
+      "kill_streak": 0,
+      "team_name": "radiant",
+      "gold": 80,
+      "gold_reliable": 0,
+      "gold_unreliable": 80,
+      "gpm": 420,
+      "xpm": 999
+    },
+  "hero": {
+      "id": 36,
+      "name": "npc_dota_hero_lina",
+      "level": 22,
+      "alive": false,
+      "respawn_seconds": 120,
+      "buyback_cost": 130,
+      "buyback_cooldown": 0,
+      "health": 420,
+      "max_health": 503,
+      "health_percent": 85,
+      "mana": 100,
+      "max_mana": 338,
+      "mana_percent": 35,
+      "silenced": false,
+      "stunned": false,
+      "disarmed": false,
+      "magicimmune": false,
+      "hexed": false,
+      "muted": false,
+      "break": false,
+      "has_debuff": false
+    },
+  "abilities": {
+      "ability0": {
+            "name": "necrolyte_death_pulse",
+            "level": 1,
+            "can_cast": true,
+            "passive": false,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability1": {
+            "name": "necrolyte_heartstopper_aura",
+            "level": 0,
+            "can_cast": false,
+            "passive": true,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability2": {
+            "name": "necrolyte_sadist",
+            "level": 0,
+            "can_cast": false,
+            "passive": true,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": false
+          },
+      "ability3": {
+            "name": "necrolyte_reapers_scythe",
+            "level": 0,
+            "can_cast": false,
+            "passive": false,
+            "ability_active": true,
+            "cooldown": 0,
+            "ultimate": true
+          },
+      "attributes": {
+            "level": 0
+          }
+    },
+  "items": {
+      "slot0": {
+            "name": "item_branches",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false
+          },
+      "slot1": {
+            "name": "item_mantle",
+            "passive": true
+          },
+      "slot2": {
+            "name": "item_clarity",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 1
+          },
+      "slot3": {
+            "name": "item_tpscroll",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 1
+          },
+      "slot4": {
+            "name": "empty",
+          },
+      "slot5": {
+            "name": "item_tango",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false,
+            "charges": 3
+          },
+      "stash0": {
+            "name": "item_circlet",
+            "passive": true
+          },
+      "stash1": {
+            "name": "item_branches",
+            "can_cast": true,
+            "cooldown": 0,
+            "passive": false
+          },
+      "stash2": {
+            "name": "empty"
+          },
+      "stash3": {
+            "name": "empty"
+          },
+      "stash4": {
+            "name": "empty"
+          },
+      "stash5": {
+            "name": "empty"
+          }
+    }
+}
+}
+,"clock_time":-39};
+content.innerHTML = template(sample);
